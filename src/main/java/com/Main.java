467c8af6e776a854f7a8763e9bee56f56b2c7333
jiseopshin <wltjq1203@icloud.com>
2025-05-22 11:04:26 +0900

    4단계

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,7 +1,36 @@
 package com;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        ArrayList<WiseSaying> wiseSayingList = new ArrayList<>();
+
+        System.out.println("== 명언 앱 ==");
+
+        while (true) {
+            System.out.print("명령) ");
+            String cmd;
+            cmd = sc.nextLine().trim();
+            if (cmd.equals("종료")) {
+                break;
+            }
+
+            switch (cmd) {
+                case "종료":
+                    return;
+                case "등록":
+                    System.out.print("명언 : ");
+                    String wiseSayingContent = sc.nextLine().trim();
+                    System.out.print("작가 : ");
+                    String wiseSayingAuthor = sc.nextLine().trim();
+                    wiseSayingList.add(new WiseSaying(wiseSayingContent, wiseSayingAuthor));
+                    System.out.printf("%d번 명언이 등록되었습니다.\n", wiseSayingList.size());
+                    break;
+            }
+        }
+        sc.close();
     }
 }
---
+++ src/main/java/com/WiseSaying.java
@@ -0,0 +1,11 @@
+package com;
+
+public class WiseSaying {
+    String content;
+    String author;
+
+    public WiseSaying(String content, String author) {
+        this.content = content;
+        this.author = author;
+    }
+}
