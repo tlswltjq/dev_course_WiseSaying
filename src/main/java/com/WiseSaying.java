01dc770685a4885ef3c1cd62f338ec68bae91363
jiseopshin <wltjq1203@icloud.com>
2025-05-22 11:48:50 +0900

    6, 7단계

--- src/main/java/com/WiseSaying.java
+++ src/main/java/com/WiseSaying.java
@@ -1,16 +1,18 @@
 package com;
 
 public class WiseSaying {
+    Integer id;
     String content;
     String author;
 
-    public WiseSaying(String content, String author) {
+    public WiseSaying(Integer id, String content, String author) {
+        this.id = id;
         this.content = content;
         this.author = author;
     }
 
     @Override
     public String toString() {
-        return author + " / " + content;
+        return id + " / " + author + " / " + content;
     }
 }
--- src/main/java/com/WiseSayingApp.java
+++ src/main/java/com/WiseSayingApp.java
@@ -2,12 +2,11 @@ package com;
 
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.stream.IntStream;
 
 public class WiseSayingApp {
     static ArrayList<WiseSaying> wiseSayingList = new ArrayList<>();
 
-    public static void start(){
+    public static void start() {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("== 명언 앱 ==");
@@ -28,18 +27,31 @@ public class WiseSayingApp {
                     String wiseSayingContent = sc.nextLine().trim();
                     System.out.print("작가 : ");
                     String wiseSayingAuthor = sc.nextLine().trim();
-                    wiseSayingList.add(new WiseSaying(wiseSayingContent, wiseSayingAuthor));
-                    System.out.printf("%d번 명언이 등록되었습니다.\n", wiseSayingList.size());
+                    if (wiseSayingList.isEmpty()) {
+                        wiseSayingList.add(new WiseSaying(1, wiseSayingContent, wiseSayingAuthor));
+                    } else {
+                        wiseSayingList.add(new WiseSaying(wiseSayingList.getLast().id + 1, wiseSayingContent, wiseSayingAuthor));
+                    }
+                    System.out.printf("%d번 명언이 등록되었습니다.\n", wiseSayingList.getLast().id);
                     break;
                 case "목록":
                     System.out.println("번호 / 작가 / 명언");
                     System.out.println("-----------------------------");
-                    IntStream.range(0, wiseSayingList.size())
-                            .forEach(i -> {
-                                WiseSaying ws = wiseSayingList.get(i);
-                                System.out.printf("%d / %s / %s\n", i + 1, ws.author, ws.content);
-                            });
+                    wiseSayingList.forEach(System.out::println);
                     break;
+                case "삭제":
+                    System.out.print("?id = ");
+                    int willingDeleteId = sc.nextInt();
+                    sc.nextLine();
+                    boolean isRemoved = wiseSayingList.removeIf(wiseSaying ->   // removeIf() 각 컬랙션에 대해 함수형 인터페이스를 사용해 true를 반환하는 요소를 제거하며 제거한 요소가 있다면 true, 그렇지 않다면 false를 반환한다.
+                            wiseSaying.id.equals(willingDeleteId));
+                    if (!isRemoved) {
+                        System.out.printf("%d번 명언은 존재하지 않습니다.\n", willingDeleteId);
+                    } else {
+                        System.out.printf("%d번 명언이 삭제되었습니다.\n", willingDeleteId);
+                    }
+                    break;
+
             }
         }
         sc.close();
